b9eccdfb859fc0eae69c608052950a9b058de5e8
Basia <b.kumor@yahoo.com>
2020-10-05 17:06:02 +0200

    list styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
