23b646df72534de12a7034a6e819e19b38bd5b72
Basia <b.kumor@yahoo.com>
2020-10-07 14:50:18 +0200

    calc total in header

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -10,6 +10,7 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 export class MainPageComponent implements OnInit {
 
   budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  totalBudget: number = 0;
 
   constructor() { }
 
@@ -18,11 +19,13 @@ export class MainPageComponent implements OnInit {
 
   addItem(newItem:BudgetItem) {
     this.budgetItems.push(newItem);
+    this.totalBudget += newItem.amount;
   }
 
   deleteItem(item:BudgetItem) {
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
+    this.totalBudget -= item.amount;
   }
 
 }
